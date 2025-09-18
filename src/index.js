c7e5a241d69c5c2b9ec19c72ef77d05e2bc2cbda
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2025-09-18 20:32:40 +0530

    add custom api response and so on

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { dbConnect } from "./DB/dbConnection.js";
-dotenv.config({path: "./.env"});
+import { app } from "./app.js";
+dotenv.config({ path: "./.env" });
 
-// DB connection 
+// DB connection
+
+const port = process.env.PORT || 4000;
 dbConnect()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running at port : ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB connection Error", err);
+    });
